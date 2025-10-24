e7b64d279c7fb63d2afcfa2d05ffddfd08bcc62d
Akshay Chandra Panicker <129216864+Akshay-max-234@users.noreply.github.com>
2025-10-24 09:27:50 +0100

    Add files via upload

---
+++ lambda.py
@@ -0,0 +1,5 @@
+sqr=lambda a: a*a
+n=int(input("Enter a number:"))
+print("Square of number is",sqr(n))
+
+    
---
+++ sumlambda.py
@@ -0,0 +1,4 @@
+sum=lambda a,b:a+b
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+print("Sum:",sum(a,b))

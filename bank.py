3f0ae8e6c5f4455a0ae20d0d6d08c0344a277047
Akshay Chandra Panicker <129216864+Akshay-max-234@users.noreply.github.com>
2026-02-14 19:21:15 +0530

    Add files via upload

--- bank.py
+++ bank.py
@@ -1,27 +1,22 @@
 class Bankacc:
-    def getdata(self, accno, name, acc_type, balance):
-        self.accno = accno
-        self.name = name
-        self.acc_type = acc_type
-        self.balance = balance
-    def deposit(self, amount):
-        self.balance += amount
-        print("Amount deposited:", amount)
-        print("Total balance:", self.balance)
-    def withdraw(self, amount):
-        if self.balance < amount:
-            print("Insufficient Balance")
-        else:
-            self.balance -= amount
-            print("Amount withdrawn:", amount)
-            print("Balance after withdrawal:", self.balance)
-accno = int(input("Enter the account no: "))
-name = input("Enter the name: ")
-acc_type = input("Enter the type: ")
-balance = float(input("Enter the initial balance: "))
-acc = Bankacc()
-acc.getdata(accno, name, acc_type, balance)
-amount = float(input("Enter amount to deposit: "))
-acc.deposit(amount)
-amount = float(input("Enter amount to withdraw: "))
-acc.withdraw(amount)
+    def __init__(self,acccno,name,acctype,balance):
+        self.accno=acccno
+        self.name=name
+        self.acctype=acctype
+        self.balance=balance
+    def deposit(self,amount):
+        self.balance+=amount
+    def withdraw(self,amount):
+      if self.balance>=amount:
+        self.balance-=amount
+      else:
+         print("insufficent balance")
+    def show(self):
+       print("ACCOUNT NO:",self.accno)
+       print("NAME:",self.name)
+       print("ACCOUNT TYPE:",self.acctype)
+       print("BALANCE:",self.balance)
+acc=Bankacc(1234,"Akshay","Current",20000)
+acc.deposit(3000)
+acc.withdraw(1000)
+acc.show()
